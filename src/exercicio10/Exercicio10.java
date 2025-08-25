ee9663a0d7a3f305f0812efffbd7df846d98996f
Lightxzxz <pedro.7700151@gmail.com>
2025-08-25 14:49:09 -0300

    exercicio10 finalizado

--- src/exercicio10/Exercicio10.java
+++ src/exercicio10/Exercicio10.java
@@ -2,6 +2,16 @@ package exercicio10;
 
 public class Exercicio10 {
     public static void main(String[] args) {
+        Funcionario funcionario = new Funcionario("Pedro", 2, 2300.00f);
 
+        System.out.println("INSS: " + funcionario.calcularInss());
+
+        funcionario.calcularAumentoSalarial(10);
+
+        System.out.println("Salário após aumento: " + funcionario.salario);
+
+        System.out.println("Imposto de renda: " + funcionario.calcularImpostoRenda());
+
+        System.out.println("Valor mensal do IRPF: " + funcionario.calcularIRPF());
     }
 }
--- src/exercicio10/Funcionario.java
+++ src/exercicio10/Funcionario.java
@@ -11,17 +11,36 @@ public class Funcionario {
         this.salario = salario;
     }
 
-    public float calcularAumentoSalarial(){
-
+    public float calcularAumentoSalarial(double percentual){
+        salario += salario * (percentual / 100);
+        return salario;
     }
 
     public float calcularInss(){
         if(salario <= 2545.00f){
-            
+            return 0.06f * salario;
+        }else{
+            return 0.1f * salario;
         }
     }
 
     public float calcularImpostoRenda(){
-        0
+        int isento = 0;
+
+        if(salario <= 1903.98f){
+            return isento;
+        } else if (salario >= 1903.99f && salario <= 2826.65f) {
+            return 0.075f * salario;
+        } else if (salario >= 2826.66f && salario <= 3751.05f) {
+            return 0.15f * salario;
+        } else if (salario >= 3751.06f && salario <= 4664.68f) {
+            return 0.225f * salario;
+        }else {
+            return 0.275f * salario;
+        }
+    }
+
+    public float calcularIRPF(){
+        return calcularImpostoRenda() - ( 545.00f * numFilhos);
     }
 }
